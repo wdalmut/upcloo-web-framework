082d8b71398d7d637a080154d51477e18c34795d
Walter Dal Mut <walter.dalmut@gmail.com>
2013-12-30 15:04:09 +0100

    Added get response tests

--- tests/UpCloo/App/EngineTest.php
+++ tests/UpCloo/App/EngineTest.php
@@ -4,6 +4,8 @@ namespace UpCloo\App;
 use Zend\EventManager\EventManager;
 use Zend\ServiceManager\ServiceManager;
 
+use Zend\Http\PhpEnvironment\Response;
+
 class EngineTest extends \PHPUnit_Framework_TestCase
 {
     private $engine;
@@ -18,5 +20,17 @@ class EngineTest extends \PHPUnit_Framework_TestCase
     public function testGetTheDefaultRequest()
     {
         $this->assertInstanceOf("Zend\\Http\\PhpEnvironment\\Request", $this->engine->request());
+    }
+
+    public function testGetTheDefaultResponse()
+    {
+        $this->assertInstanceOf("Zend\\Http\\PhpEnvironment\\Response", $this->engine->response());
+    }
+
+    public function testMyResponse()
+    {
+        $response = new Response();
+        $this->engine->setResponse($response);
+        $this->assertSame($response, $this->engine->response());
     }
 }
