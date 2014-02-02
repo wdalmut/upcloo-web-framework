ad845f79c82db2147d5807140b9943f7b7531a3a
Walter Dal Mut <walter.dalmut@gmail.com>
2014-02-02 12:07:55 +0100

    Test events are called in order

--- tests/UpCloo/App/EngineTest.php
+++ tests/UpCloo/App/EngineTest.php
@@ -6,6 +6,8 @@ use Zend\ServiceManager\ServiceManager;
 
 use Zend\Http\PhpEnvironment\Response;
 
+use Zend\Mvc\Router\Http\RouteMatch;
+
 class EngineTest extends \PHPUnit_Framework_TestCase
 {
     private $engine;
@@ -32,5 +34,69 @@ class EngineTest extends \PHPUnit_Framework_TestCase
         $response = new Response();
         $this->engine->setResponse($response);
         $this->assertSame($response, $this->engine->response());
+    }
+
+    public function testErrorEventsAreCalledInOrder()
+    {
+        $eventManager = new EventManager();
+        $serviceManager = new ServiceManager();
+
+        $engine = new Engine();
+        $engine->setServiceManager($serviceManager);
+        $engine->setEventManager($eventManager);
+
+        $result = "";
+        $eventManager->attach("begin", function() use (&$result) {
+            $result .= "b";
+        });
+        $eventManager->attach("route", function() use (&$result) {
+            $result .= "r";
+        });
+        $eventManager->attach("404", function() use (&$result) {
+            $result .= "4";
+        });
+        $eventManager->attach("renderer", function() use (&$result) {
+            $result .= "re";
+        });
+        $eventManager->attach("finish", function() use (&$result) {
+            $result .= "f";
+        });
+
+        $engine->run();
+        $this->assertEquals("br4ref", $result);
+    }
+
+    public function testEventsAreCalledInOrder()
+    {
+        $eventManager = new EventManager();
+        $serviceManager = new ServiceManager();
+
+        $engine = new Engine();
+        $engine->setServiceManager($serviceManager);
+        $engine->setEventManager($eventManager);
+
+        $result = "";
+        $eventManager->attach("begin", function() use (&$result) {
+            $result .= "b";
+        });
+        $eventManager->attach("route", function() use (&$result) {
+            $result .= "r";
+            return new RouteMatch([]);
+        });
+        $eventManager->attach("pre.fetch", function() use (&$result) {
+            $result .= "p";
+        });
+        $eventManager->attach("execute", function() use (&$result) {
+            $result .= "e";
+        });
+        $eventManager->attach("renderer", function() use (&$result) {
+            $result .= "re";
+        });
+        $eventManager->attach("finish", function() use (&$result) {
+            $result .= "f";
+        });
+
+        $engine->run();
+        $this->assertSame("brperef", $result);
     }
 }
